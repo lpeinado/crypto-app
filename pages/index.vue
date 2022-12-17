67bf63ac76300358cf4b263fce37deeedb9b3dae
Lluis Peinado <lpeinado@stelladot.com>
2022-12-17 23:58:24 +0100

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template> 
+  <div v-for="currency in data.data" :key="currency">
+    <ul class="ticker">
+      <li class="property">A currency:</li>
+      <li class="property">{{ currency.name}}</li>
+      <li class="property">{{ currency.symbol }}</li>
+      <li class="property">{{ currency.price_usd }}</li>
+      <li class="property">
+        <NuxtLink :to="'currency/'+ currency.id">{{ currency.id }}</NuxtLink>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+  const {data} = await useFetch('/api/tickers?limit=10')
+</script>
+
+<style scoped>
+.ticker {
+  display: inline;
+}
+.property{
+    display: inline;
+  }
+</style>
